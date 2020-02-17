d7e17e355677d74817c3ff61ee39adb1ad1f199b
emijay <hilmi.johari@yahoo.com.sg>
2020-02-17 09:52:57 +0800

    refactored Map component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import { Map, TileLayer } from "react-leaflet";
+import MapComponent from "./components/Map";
 
-const App = () => {
+const App = props => {
   return (
     <div>
       <div>
-        <h1>React Leaftlet</h1>
+        <h1>React Leaftlet Project</h1>
       </div>
-      <div className="leaflet-container">
-        <Map center={[1.34, 103.83]} zoom={12}>
-          <TileLayer
-            url="https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}"
-            attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
-            maxZoom="18"
-            id="mapbox/streets-v11"
-            accessToken="pk.eyJ1IjoiZW1pamF5IiwiYSI6ImNrNmtqbXN6YzA0cHUzcnJyYWN4anRjMTEifQ.UIQ_UNwanG23WQJ5PvCYuw"
-          />
-        </Map>
-      </div>
+      <MapComponent />
     </div>
   );
 };
---
+++ src/components/Map.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+
+export default class MapComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: 1.357107,
+      lng: 103.8194992,
+      zoom: 12
+    };
+  }
+
+  render() {
+    const position = [this.state.lat, this.state.lng];
+    return (
+      <div className="leaflet-container">
+        <Map center={position} zoom={this.state.zoom}>
+          <TileLayer
+            url="https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}"
+            attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+            maxZoom="18"
+            id="mapbox/streets-v11"
+            accessToken="pk.eyJ1IjoiZW1pamF5IiwiYSI6ImNrNmtqbXN6YzA0cHUzcnJyYWN4anRjMTEifQ.UIQ_UNwanG23WQJ5PvCYuw"
+          />
+        </Map>
+      </div>
+    );
+  }
+}
